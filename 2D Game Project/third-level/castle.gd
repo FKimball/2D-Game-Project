72236757299bd672fa997b10de4bd67c3ccb7db2
FKimball <156947100+FKimball@users.noreply.github.com>
2025-03-11 00:23:26 -0400

    Updated Level 3

---
+++ 2D Game Project/third-level/castle.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+@onready var points_label: Label = %Label 
+var points = 0
+
+func add_point(value:int):
+	points += value
+	$AudioStreamPlayer2D.play()
+	points_label.text = "Coins: " + str(points)
