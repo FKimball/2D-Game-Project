f10272e043373a6d8efcfaa2a7a128a8e2402e6f
FKimball <156947100+FKimball@users.noreply.github.com>
2025-03-11 11:52:40 -0400

    Added to FINAL version

---
+++ 2D GAME FINAL/Third_Level/character_body_2d.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+
+@export var spawn_point: Vector2
+
+@onready var sound = $AudioStreamPlayer2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+func _ready():
+	spawn_point = Vector2(1016, 44) 
+	global_position = spawn_point
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		$AnimatedSprite2D.play("jump")
+
+	# Get the input direction and handle the movement/deceleration.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		if direction == -1:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+		velocity.x = direction * SPEED
+		$AnimatedSprite2D.play("run")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.play("idle")
+
+	move_and_slide()
+
+# Detect collision with the enemy
+func _on_Enemy_area_entered(area: Area2D):
+	if area.is_in_group("enemy"):  # Assuming the enemy is in the "enemy" group
+		global_position = spawn_point  # Reset the player position to spawn point
---
+++ 2D GAME FINAL/Third_Level/scoin.gd
@@ -0,0 +1,13 @@
+extends AnimatedSprite2D
+
+@export var coin_value: int = 1
+@onready var level = get_tree().get_first_node_in_group("level")
+
+func _ready() -> void:
+	play("spin")         # Play the "spin" animation
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "CharacterBody2D":
+		level.add_point(coin_value)
+		queue_free()
+		
