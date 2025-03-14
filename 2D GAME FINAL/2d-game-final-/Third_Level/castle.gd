e7ce1bf0f14ee0c8bb10053670f84bed3b28a49e
FKimball <156947100+FKimball@users.noreply.github.com>
2025-03-14 11:39:58 -0400

    Level 2 Updates

--- 2D GAME FINAL/2d-game-final-/Third_Level/castle.gd
+++ 2D GAME FINAL/2d-game-final-/Third_Level/castle.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
 @onready var points_label: Label = %Label 
-var points = 40
+var points = 0
 
+func _ready():
+	points = Globals.coins
+	points_label.text = "Coins: " + str(points)
+	
 func add_point(value:int):
 	points += value
+	Globals.coins = points
 	$AudioStreamPlayer2D.play()
 	points_label.text = "Coins: " + str(points)
