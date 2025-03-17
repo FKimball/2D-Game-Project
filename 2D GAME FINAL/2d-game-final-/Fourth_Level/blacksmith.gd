43ed3b1fef7660cb8572a01f03b1eb6589a345ce
Anton M <antonmaironi@gmail.com>
2025-03-16 23:17:41 -0400

    Added first NPC of last level
    Need to do four more and add the win/loss conditions

---
+++ 2D GAME FINAL/2d-game-final-/Fourth_Level/blacksmith.gd
@@ -0,0 +1,31 @@
+extends AnimatedSprite2D
+
+var in_blacksmith = false
+var interactable = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if in_blacksmith == true && interactable == true:
+		if Input.is_action_just_pressed("ui_up"):
+			play("happy")
+			$"../blacksmith_text".text = "___________________________________
+|  25 gold coins just for me!?    |
+|  Thank you so much!               |
+|________________________________ |
+" + "           V"
+			interactable == false
+			$"../Icon/Reminder".visible = false
+			$"../Icon".pouches_left -= 1
+
+func _on_area_2d_blacksmith_body_entered(body: Node2D) -> void:
+	$"../blacksmith_text".visible = true
+	in_blacksmith = true
+	
+func _on_area_2d_blacksmith_body_exited(body: Node2D) -> void:
+	$"../blacksmith_text".visible = false # Replace with function body.
+	in_blacksmith = false
--- 2D GAME FINAL/2d-game-final-/Fourth_Level/icon.gd
+++ 2D GAME FINAL/2d-game-final-/Fourth_Level/icon.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 400.0
+const JUMP_VELOCITY = -450.0
 
 var in_workbench = false
 var is_jumping = false
+var pouches_left = 5
 
 func _physics_process(_delta: float) -> void:
 	# Add the gravity.
@@ -31,7 +32,7 @@ func _ready() -> void:
 		
 func _process(_delta: float) -> void: 
 	if in_workbench == true:
-		if Input.is_action_just_pressed("ui_accept") || Input.is_action_just_pressed("ui_up"):
+		if Input.is_action_just_pressed("ui_up"):
 			get_tree().change_scene_to_file("res://Fourth_Level/coindropper.tscn")
 	
 	var direction := Input.get_axis("ui_left", "ui_right")
@@ -52,6 +53,8 @@ func _process(_delta: float) -> void:
 		$Timer.start()
 		$AnimatedSprite2D.play("jump")
 	
+	$Pouches.text = "Pouches left to distribute: " + str(pouches_left) + "/5"
+	
 func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	in_workbench = true
 
@@ -62,5 +65,5 @@ func _on_timer_timeout() -> void:
 	is_jumping = false # Replace with function body.
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_area_2d_body_entered(_body: Node2D) -> void:
 	$Label.visible = true # Replace with function body.
